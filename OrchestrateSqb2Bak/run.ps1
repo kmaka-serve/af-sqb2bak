a16929e8ce977521d51356fefa26a9b6fae6cb2c
kmaka-serve <kmaka@softserveinc.com>
2025-09-05 15:01:29 +0300

    Add files via upload

---
+++ OrchestrateSqb2Bak/run.ps1
@@ -0,0 +1,13 @@
+param($Context)
+
+$in = $Context.Input
+
+# 1) Викликаємо activity і ЯВНО чекаємо результат
+$task   = Invoke-DurableActivity -FunctionName 'Worker_BCRH' -Input $in
+$result = Wait-DurableTask -Task $task
+
+# 2) Щоб одразу бачити відповідь у статусі
+Set-DurableCustomStatus -Value $result
+
+# 3) Повертаємо в output (буде видно в statusQueryGetUri)
+return $result
